dad32aff1dc1b1ae4960e4140035183ec03aa101
andersongm <and.gmartins@gmail.com>
2019-11-27 10:46:30 -0300

    adjust footer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
     jQuery.support.cors = true;
     var urlBase = "http://localhost:3333/students/2"
-    console.log('Carregou');
-    // $.get(urlBase, function(resultado){
-    //  console.log(resultado);
-    // });
-
+    
     $.ajax({
         url: urlBase,
         //headers: { 'Access-Control-Allow-Origin': 'http://localhost:5501' },
