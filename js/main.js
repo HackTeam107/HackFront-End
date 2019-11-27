bded6308d512293176a2dc6cd2d446ed6b722e7a
andersongm <and.gmartins@gmail.com>
2019-11-27 11:17:50 -0300

    adjust agm

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function () {
     jQuery.support.cors = true;
-    var urlBase = "http://localhost:3333/students/2"
+    var urlBase = "https://fiindev.herokuapp.com/services/api/v1/students/1"
     
     $.ajax({
         url: urlBase,
-        //headers: { 'Access-Control-Allow-Origin': 'http://localhost:5501' },
+        headers: { 'Access-Control-Allow-Origin': 'http://127.0.0.1:5502' },
         method: 'GET',
         type: 'jsonp',
         success: function (jsondata) {
             console.log(jsondata)
             $("#user_logged").html(jsondata.name);
-
         }
     });
 
